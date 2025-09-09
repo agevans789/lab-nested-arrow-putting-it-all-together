d0759b6abbcd11760613f9ea11894fb8ef4947de
Ann Grace Evans <139709739+agevans789@users.noreply.github.com>
2025-09-08 23:57:09 -0400

    Update index.js
    Completing assignment with account lockout

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ function createLoginTracker(userInfo) {
   let attemptCount = 0;
   const passwordAttempt = (passwordAttempt) => {
     attemptCount += 1;
-  }
+  };
   if (passwordAttempt === userInfo.password && attemptCount <= 3) {
     return "Login successful";
-  } else if (passwordAttempt !== userInfo.password && attemptCount <=3) {
+  }; else if (passwordAttempt !== userInfo.password && attemptCount <=3) {
     return "Login failed";
-  }
+  };
+  else if (attemptCount > 3) {
+    return "Account locked due to too many failed login attempts";
+};
 }
 
 
@@ -19,4 +22,5 @@ module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 
 };
+
 
