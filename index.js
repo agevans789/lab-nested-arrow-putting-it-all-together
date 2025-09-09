c1dfd26066ba3acab444fd3ac238f389ec526ef1
Ann Grace Evans <139709739+agevans789@users.noreply.github.com>
2025-09-08 23:49:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-
+function createLoginTracker(userInfo) {
+  const userInfo = {
+    username: userInfo.username,
+    password: userInfo.password
+  };
+  let attemptCount = 0;
+  const passwordAttempt = (passwordAttempt) => {
+    attemptCount += 1;
+  }
+  if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+    return "Login successful";
+  } else {
+    return "Login failed";
+  }
+}
 
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+
 };
